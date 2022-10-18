f1797eae1e40de6a38b13ed371c7b0bd16c1f4c7
liuh <counull@163.com>
2022-10-19 03:46:17 +0800

    Surrounder rotate

--- Assets/Scripts/WeaponBehavior/MagicStoneBehavior.cs
+++ Assets/Scripts/WeaponBehavior/MagicStoneBehavior.cs
@@ -1,7 +1,36 @@
-﻿using GameController;
+﻿using System;
+using System.Collections;
+using GameController;
+using UnityEngine;
 
 namespace WeaponBehavior {
-    public class MagicStoneBehavior:WeaponController {
-        
+    public class MagicStoneBehavior : WeaponController {
+        public float angularVelocity;
+
+
+        private void Start() {
+            StartCoroutine(nameof(Surround));
+        }
+
+        IEnumerator Surround() {
+            while (true) {
+                transform.localPosition = Quaternion.AngleAxis(angularVelocity * Time.deltaTime,Vector3.forward) *
+                                          transform.localPosition;
+                yield return null;
+            }
+        }
+
+
+        private void OnCollisionEnter2D(Collision2D col) {
+            if (col.gameObject.CompareTag("Enemy")) {
+                Debug.Log(col.gameObject.name + "get hurt");
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            if (col.gameObject.CompareTag("Enemy")) {
+                Debug.Log(col.gameObject.name + "get hurt");
+            }
+        }
     }
 }
